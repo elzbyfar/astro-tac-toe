a650b87bf40b038f733b343483eeaf70d4363f85
Luis Alejo <lou.m.alejo@gmail.com>
2023-12-18 14:25:16 -0800

    remove padding from exit button

--- src/components/Game/ExitButton.tsx
+++ src/components/Game/ExitButton.tsx
@@ -9,6 +9,8 @@ import {
   setActiveRound,
 } from "../../lib/state";
 import { INITIAL_RESULT, INITIAL_STATS } from "../../lib/constants";
+import { stylesReducer } from "../../lib/utils";
+
 export default function ExitButton() {
   const activeGame = useStore(activeGameStore);
   const stats = useStore(statsStore);
@@ -24,13 +26,19 @@ export default function ExitButton() {
       difficulty: stats.difficulty,
     });
   }
+
+  const className = {
+    button:
+      "text-[12px] text-rose-400 mt-4 py-0 px-4 rounded-xl transition-all duration-200 ease-in-out",
+    buttonMd: "md:py-2",
+    buttonVisibility: `${activeGame ? "flex" : "hidden"}`,
+    buttonHoverLg: "lg:hover:bg-rose-400 lg:hover:text-white",
+  };
+
+  const styles = stylesReducer(className);
+
   return (
-    <button
-      className={`${
-        activeGame ? "flex" : "hidden"
-      } text-[12px] text-rose-400 mt-4 hover:bg-rose-400 hover:text-white py-2 px-4 rounded-xl transition-all duration-200 ease-in-out`}
-      onClick={handleExit}
-    >
+    <button className={styles("button")} onClick={handleExit}>
       EXIT GAME
     </button>
   );
